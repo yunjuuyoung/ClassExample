668ae5f40013d2d4bc211cc48c75144a0041e58f
juyoung07 <yunju5090@gmail.com>
2025-06-13 10:03:57 +0900

    상속 관계에서의 생성자 호출 예제

--- Program.cs
+++ Program.cs
@@ -8,8 +8,33 @@ namespace ClassExample
 {
     class Program
     {
+        class Parent
+        {
+            public Parent() { Console.WriteLine("부모 생성자"); }
+            public Parent(string name) { Console.WriteLine("Parent(string name)"); }
+            public Parent(int param) { Console.WriteLine("Parent(int param)"); }
+        }
+
+        private class Child: Parent
+        {
+            public Child(): base("child")
+            {
+                Console.WriteLine("자식 생성자");
+            }
+            public Child(string name): base(name)
+            {
+                Console.WriteLine("Child(string name)");
+            }
+            public Child(int param) : base(param)
+            {
+                Console.WriteLine("Child(int param)");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Child child1 = new Child("abc");
+            Child child2 = new Child();
         }
     }
 }
